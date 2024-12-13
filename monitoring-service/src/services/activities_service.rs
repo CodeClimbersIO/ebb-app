6921dba49d2c1755192b3de4aeb36e6c737b53a9
Paul Hovley <rphovley@gmail.com>
2024-12-13 12:37:38 -0700

    remove dead code

--- monitoring-service/src/services/activities_service.rs
+++ monitoring-service/src/services/activities_service.rs
@@ -1,34 +1,28 @@
-use std::time::Duration;
-
-// activity service provides an interface to read/write to the activities table
 use monitor::{EventCallback, KeyboardEvent, MouseEvent, WindowEvent};
-use parking_lot::Mutex;
-use tokio::{sync::mpsc, time::Instant};
+use tokio::sync::mpsc;
 
 use crate::db::{activities_repo::ActivitiesRepo, db_manager, models::Activity};
 #[derive(Clone)]
 pub struct ActivityService {
     activities_repo: ActivitiesRepo,
     event_sender: mpsc::UnboundedSender<ActivityEvent>,
-    last_keyboard_time: std::sync::Arc<Mutex<Instant>>,
-    last_mouse_time: std::sync::Arc<Mutex<Instant>>,
 }
 
 enum ActivityEvent {
-    Keyboard(bool),
-    Mouse(bool),
+    Keyboard(),
+    Mouse(),
     Window(WindowEvent),
 }
 
 impl EventCallback for ActivityService {
-    fn on_keyboard_events(&self, events: Vec<KeyboardEvent>) {
-        if let Err(e) = self.event_sender.send(ActivityEvent::Keyboard(true)) {
+    fn on_keyboard_events(&self, _: Vec<KeyboardEvent>) {
+        if let Err(e) = self.event_sender.send(ActivityEvent::Keyboard()) {
             eprintln!("Failed to send keyboard event: {}", e);
         }
     }
 
-    fn on_mouse_events(&self, events: Vec<MouseEvent>) {
-        if let Err(e) = self.event_sender.send(ActivityEvent::Mouse(true)) {
+    fn on_mouse_events(&self, _: Vec<MouseEvent>) {
+        if let Err(e) = self.event_sender.send(ActivityEvent::Mouse()) {
             eprintln!("Failed to send mouse event: {}", e);
         }
     }
@@ -41,15 +35,6 @@ impl EventCallback for ActivityService {
     }
 }
 impl ActivityService {
-    fn should_send_event(now: Instant, last_time: &std::sync::Arc<Mutex<Instant>>) -> bool {
-        let mut guard = last_time.lock();
-        if now.duration_since(*guard) >= Duration::from_secs(30) {
-            *guard = now;
-            true
-        } else {
-            false
-        }
-    }
     async fn handle_keyboard_activity(&self) {
         let activity = Activity::create_keyboard_activity();
         if let Err(err) = self.save_activity(&activity).await {
@@ -74,22 +59,19 @@ impl ActivityService {
     pub fn new(pool: sqlx::SqlitePool) -> Self {
         let (sender, mut receiver) = mpsc::unbounded_channel();
         let activities_repo = ActivitiesRepo::new(pool);
-        let past = Instant::now() - Duration::from_millis(30_100); // 30.1 seconds ago
         let service = ActivityService {
             activities_repo,
             event_sender: sender,
-            last_keyboard_time: std::sync::Arc::new(parking_lot::Mutex::new(past)),
-            last_mouse_time: std::sync::Arc::new(parking_lot::Mutex::new(past)),
         };
         let service_clone = service.clone();
 
         tokio::spawn(async move {
             while let Some(event) = receiver.recv().await {
                 match event {
-                    ActivityEvent::Keyboard(_) => {
+                    ActivityEvent::Keyboard() => {
                         service_clone.handle_keyboard_activity().await;
                     }
-                    ActivityEvent::Mouse(_) => service_clone.handle_mouse_activity().await,
+                    ActivityEvent::Mouse() => service_clone.handle_mouse_activity().await,
                     ActivityEvent::Window(e) => service_clone.handle_window_activity(e).await,
                 }
             }
