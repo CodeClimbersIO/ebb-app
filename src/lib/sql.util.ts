b5f87cae7124a48f8f97c3d156b73849889f039e
Paul Hovley <rphovley@gmail.com>
2025-01-02 10:13:32 -0700

    add utils

---
+++ src/lib/sql.util.ts
@@ -0,0 +1,17 @@
+import Database, { QueryResult } from "@tauri-apps/plugin-sql";
+
+export const insert = async (db: Database, table: string, record: Record<string, string>): Promise<QueryResult> => {
+  const keys = Object.keys(record);
+  const values = Object.values(record);
+  const query = `INSERT INTO ${table} (${keys.join(', ')}) VALUES (${values.map(() => '?').join(', ')})`;
+  const result = await db.execute(query, values);
+  return result;
+}
+
+export const update = async (db: Database, table: string, record: Record<string, string>, id: string) => {
+  const keys = Object.keys(record);
+  const values = Object.values(record);
+  const query = `UPDATE ${table} SET ${keys.map((key, index) => `${key} = ?`).join(', ')} WHERE id = ?`;
+  const result = await db.execute(query, [...values, id]);
+  return result;
+}
