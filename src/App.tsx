f6341726f6360885660d6e43b62bfe933983a376
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2025-01-02 16:59:05 -0500

    this container style was messing up the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "@/components/ThemeProvider"
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <main className="container">
+      <main>
         <AppRouter />
       </main>
     </ThemeProvider>
