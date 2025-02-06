e538d726d4d67966dee1d4fcf7eee8b18861eb4e
Paul Hovley <rphovley@gmail.com>
2025-02-05 18:46:41 -0700

    limit logger

--- cc-logger/src/lib.rs
+++ cc-logger/src/lib.rs
@@ -1,13 +1,8 @@
 use backtrace::Backtrace;
 use chrono::Local;
-use colored::*;
 use std::thread;
 pub fn log(message: &str) {
-    log_base(message, 0, "blue");
-}
-
-pub fn log_indent(message: &str, indent: usize, color: &str) {
-    log_base(message, indent, color);
+    log_base(message);
 }
 
 fn get_caller_function() -> String {
@@ -34,23 +29,11 @@ fn get_caller_function() -> String {
     "unknown".to_string()
 }
 
-pub fn log_base(message: &str, indent: usize, color: &str) {
+pub fn log_base(message: &str) {
     let thread_name = thread::current().name().unwrap_or("unnamed").to_string();
-    let indent = "  ".repeat(indent);
     let caller = get_caller_function();
     let prefix = format!("{}-{}", thread_name, caller);
     let current_time = Local::now().format("%Y-%m-%d %H:%M:%S");
-    let colored_prefix = match color.to_lowercase().as_str() {
-        "red" => prefix.red(),
-        "green" => prefix.green(),
-        "yellow" => prefix.yellow(),
-        "blue" => prefix.blue(),
-        "magenta" => prefix.magenta(),
-        "cyan" => prefix.cyan(),
-        _ => prefix.blue(), // default to blue if color is not recognized
-    };
-    println!(
-        "[{}]{}{}: {}",
-        current_time, indent, colored_prefix, message
-    );
+
+    println!("[{}]{}: {}", current_time, prefix, message);
 }
--- cc-logger/src/main.rs
+++ cc-logger/src/main.rs
@@ -1,10 +1,8 @@
 fn test_log() {
     cc_logger::log("Hello, world!");
-    cc_logger::log_indent("Hello, world!", 3, "red");
 }
 
 fn main() {
     // do_some_work();
     test_log();
-    cc_logger::log_indent("Hello, world!", 3, "red");
 }
