d0beecfb2a3de41c27a1eabde096adf8bc7c54ab
Nathan Covey <156732802+nathancovey@users.noreply.github.com>
2025-03-27 14:48:32 -0400

    change spotify callback url

--- src/hooks/useDeepLinkAuth.ts
+++ src/hooks/useDeepLinkAuth.ts
@@ -52,7 +52,7 @@ export const useDeepLinkAuth = () => {
         const state = searchParams.get('state')
         if (code && state) {
           await SpotifyAuthService.handleCallback(code, state)
-          navigate('/settings', { replace: true })
+          navigate('/start-flow', { replace: true })
           window.location.reload()
           return
         }
