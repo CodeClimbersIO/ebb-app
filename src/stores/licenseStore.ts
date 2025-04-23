2d8753addbfe0890cae9ee5c04fe2ebfc56e0687
Paul Hovley <rphovley@gmail.com>
2025-04-22 18:52:24 -0600

    made better error message

--- src/stores/licenseStore.ts
+++ src/stores/licenseStore.ts
@@ -76,7 +76,7 @@ export const useLicenseStore = create<LicenseStoreState>()(
           )
           .subscribe((status, err) => {
             if (status === 'CHANNEL_ERROR') {
-              logAndToastError(`Subscription timed out for user ${userId}.`)
+              logAndToastError(`Subscription errored out for user ${userId}.`, err)
               error(`Subscription error for user ${userId}: ${err}`)
             } else if (status === 'TIMED_OUT') {
               logAndToastError(`Subscription timed out for user ${userId}.`)
